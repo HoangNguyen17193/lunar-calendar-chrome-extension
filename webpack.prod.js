078f65741fd27a34d593d7e538c4c48fea449bec
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-10-28 15:53:13 +0700

    add webpack config for production mode

---
+++ webpack.prod.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const common = require('./webpack.config.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+  ]
+});
